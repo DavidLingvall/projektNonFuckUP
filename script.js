ea5d1a4b7053c7be21ac9f296f3a6fab685353de
DavidLingvall <36515541+DavidLingvall@users.noreply.github.com>
2018-09-25 13:20:17 +0200

    David

---
+++ script.js
@@ -0,0 +1,37 @@
+var images = new Array();
+images[0] = "bild1.jpg";
+images[1] = "bild2.jpg";
+images[2] = "bild3.jpg";
+var i = 0;
+
+function bytBild(){
+  if (i < images.length) {
+    $('#bildSpel').attr('src', images[i]);
+    $('#bildSpel').fadeIn(2000);
+    i++;
+  }
+  else if (i == images.length) {
+    i=0;
+    $('#bildSpel').attr('src', images[i]);
+    $('#bildSpel').fadeIn(2000);
+    i++;
+  }
+}
+function timer(){
+  setInterval(test, 4000);
+} 
+function test(){
+  $('#bildSpel').fadeOut(2000, function(){
+    if (i < images.length) {
+      $('#bildSpel').attr('src', images[i]);
+      $('#bildSpel').fadeIn(2000);
+      i++;
+    }
+    else if (i == images.length) {
+      i=0;
+      $('#bildSpel').attr('src', images[i]);
+      $('#bildSpel').fadeIn(2000);
+      i++;
+    }
+  });
+}
